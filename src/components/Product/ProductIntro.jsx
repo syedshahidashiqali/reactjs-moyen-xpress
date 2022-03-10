ece4e73312d79a645a8b0b215c62fcf50209b9e9
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-03-10 20:32:56 +0500

    product intro mid section completed
    responsiveness is remaining

--- src/components/Product/ProductIntro.jsx
+++ src/components/Product/ProductIntro.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./ProductIntro.css";
 import {
   CarouselProvider,
@@ -9,21 +10,23 @@ import {
   Image,
   Dot,
 } from "pure-react-carousel";
-
+import { Breadcrumb, Form, Button } from "react-bootstrap";
+import { Rating } from "react-simple-star-rating";
 // dummy data
 import { dummyData } from "../../dummyData";
 
 export default function ProductIntro() {
+  const [rating, setRating] = useState(0);
   return (
     <div className="productSection">
       <div className="container-fluid">
         <div className="row">
           <div className="col-9">
-            <div className="row">
+            <div className="row mb-3">
               <div className="col-6">
                 <CarouselProvider
                   naturalSlideWidth={100}
-                  naturalSlideHeight={110}
+                  naturalSlideHeight={90}
                   totalSlides={4}
                   visibleSlides={1}
                   className="prodcarouselProvider"
@@ -31,7 +34,11 @@ export default function ProductIntro() {
                   <Slider className="prodcarouselSlider">
                     {dummyData.map((image, index) => {
                       return (
-                        <Slide index={index} className="prodcarouselSlide">
+                        <Slide
+                          index={index}
+                          key={index}
+                          className="prodcarouselSlide"
+                        >
                           <ProductCarouselItem image={image} />
                         </Slide>
                       );
@@ -58,7 +65,76 @@ export default function ProductIntro() {
                   </div>
                 </CarouselProvider>
               </div>
-              <div className="col-6"></div>
+              <div className="col-6">
+                <div className="prodIntroMid">
+                  <h2>electronics black wrist watch</h2>
+                  <div className="prodIntroMidTop my-3">
+                    <Breadcrumb>
+                      <span className="me-2 bold">Category:</span>
+                      <Breadcrumb.Item href="#">Electronics</Breadcrumb.Item>
+                      <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+                        Computer
+                      </Breadcrumb.Item>
+                      <Breadcrumb.Item active>Monitor</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <span>
+                      <span className="bold">Stock: </span> 2
+                    </span>
+                    <span className="mt-3">
+                      <span className="bold">SKU: </span> MS46891340
+                    </span>
+                  </div>
+                  <div className="prodIntroMidMid">
+                    <h2>$99.00</h2>
+                    <div className="prodIntroRating d-flex ai-c">
+                      <Rating
+                        ratingValue={rating}
+                        size={20}
+                        allowHalfIcon={true}
+                      />
+                      <span>(0 reviews)</span>
+                    </div>
+                  </div>
+                  <div className="prodIntroMidBot mt-4">
+                    <div className="prodIntroAttr prodIntroSizeAtt">
+                      <label htmlFor="">Size:</label>
+                      <Form.Select
+                        aria-label="Default select example"
+                        className="prodIntroForm"
+                      >
+                        <option>Please Select Attribute</option>
+                        <option value="12">12</option>
+                        <option value="24">24</option>
+                        <option value="32">32</option>
+                      </Form.Select>
+                    </div>
+                    <div className="prodIntroAttr prodIntroColorAtt mt-4">
+                      <label htmlFor="">Color:</label>
+                      <Form.Select
+                        aria-label="Default select example"
+                        className="prodIntroForm"
+                      >
+                        <option>Please Select Attribute</option>
+                        <option value="Gray">Gray</option>
+                        <option value="Black">Black</option>
+                        <option value="Blue">Blue</option>
+                      </Form.Select>
+                    </div>
+                  </div>
+                  <div className="prodIntroMidLast mt-4 d-flex ai-c">
+                    <a href="">
+                      <i className="fa-regular fa-heart me-3" />
+                    </a>
+                    <Button variant="primary" size="lg">
+                      <i
+                        className="fa-solid fa-cart-shopping"
+                        style={{ marginRight: "0.5rem" }}
+                      />
+                      add to cart
+                    </Button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="col-3"></div>
