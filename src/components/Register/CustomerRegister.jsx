2266a74a08b6fbc37b332fbc79e9b90bdde0ec94
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-03-22 18:24:14 +0500

    customer register component complete with responsiveness

--- src/components/Register/CustomerRegister.jsx
+++ src/components/Register/CustomerRegister.jsx
@@ -1,12 +1,61 @@
 import "./CustomerRegister.css";
+import { Form } from "react-bootstrap";
 
 export default function CustomerRegister() {
   return (
     <div className="customerRegisterWrapper">
       <div className="container">
-        <div className="row">
-          <div className="col-md-12 col-sm-12 col-12">
-            <h4>Register Customer</h4>
+        <div className="row jc-c mt-5 mb-3">
+          <div className="col-md-6 col-sm-6 col-ms-offset-3 col-sm-offset-3">
+            <div className="customerRegisterBox">
+              <div className="customerRegisterBoxTitle">
+                <h3>Register as customer</h3>
+              </div>
+              <Form>
+                <Form.Group className="mb-3">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <Form.Control type="text" placeholder="User Name" />
+                    </div>
+                  </div>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <Form.Control type="email" placeholder="Email Address" />
+                    </div>
+                  </div>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <Form.Control
+                        type="password"
+                        placeholder="Create Password"
+                      />
+                    </div>
+                  </div>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <Form.Control type="text" placeholder="Phone Number" />
+                    </div>
+                  </div>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <Form.Control
+                        type="submit"
+                        value="register"
+                        className="customerRegisterSubmitBtn"
+                      />
+                    </div>
+                  </div>
+                </Form.Group>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
