0436cdbf633223ac902b46d9bdd8e14017698071
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-03-28 20:28:02 +0500

    cart page responsiveness complete

--- src/components/Cart/CartMain.jsx
+++ src/components/Cart/CartMain.jsx
@@ -13,11 +13,11 @@ export default function CartMain() {
             <CartBreadcrumb />
           </div>
         </div>
-        <div className="row mt-5 mb-4">
+        <div className="row mt-5 mb-5">
           <div className="col-md-8 col-sm-12 col-12">
             <CartTable />
             <div className="row">
-              <div className="col-md-6 col-sm-12 col-12">
+              <div className="col-md-6 col-sm-6 col-6">
                 <div className="cartMainShopBtn">
                   <Button classNAme="d-flex jc-c ai-c">
                     <i className="fa-solid fa-arrow-left-long me-2" />
@@ -25,7 +25,7 @@ export default function CartMain() {
                   </Button>
                 </div>
               </div>
-              <div className="col-md-6 col-sm-12 col-12">
+              <div className="col-md-6 col-sm-6 col-6">
                 <div className="cartMainUpdateCartBtn d-flex jc-e">
                   <Button classNAme="d-flex jc-c ai-c">update cart</Button>
                 </div>
