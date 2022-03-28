fc7bfbd1d06125fca94d85e9a669e703304bdc66
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-03-28 19:41:19 +0500

    add two button in the bottom of table

--- src/components/Cart/CartMain.jsx
+++ src/components/Cart/CartMain.jsx
@@ -2,6 +2,7 @@ import CartBreadcrumb from "./CartBreadcrumb";
 import "./CartMain.css";
 import CartTable from "./CartTable";
 import CartTotal from "./CartTotal";
+import { Button } from "react-bootstrap";
 
 export default function CartMain() {
   return (
@@ -12,9 +13,24 @@ export default function CartMain() {
             <CartBreadcrumb />
           </div>
         </div>
-        <div className="row mt-5">
+        <div className="row mt-5 mb-4">
           <div className="col-md-8 col-sm-12 col-12">
             <CartTable />
+            <div className="row">
+              <div className="col-md-6 col-sm-12 col-12">
+                <div className="cartMainShopBtn">
+                  <Button classNAme="d-flex jc-c ai-c">
+                    <i className="fa-solid fa-arrow-left-long me-2" />
+                    continue shopping
+                  </Button>
+                </div>
+              </div>
+              <div className="col-md-6 col-sm-12 col-12">
+                <div className="cartMainUpdateCartBtn d-flex jc-e">
+                  <Button classNAme="d-flex jc-c ai-c">update cart</Button>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-md-4 col-sm-12 col-12">
             <CartTotal />
