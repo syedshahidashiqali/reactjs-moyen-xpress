08e673bbd9785625d5bd9289584ee95d580e056b
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-03-29 17:31:43 +0500

    checkout page form component complete

---
+++ src/components/Checkout/CheckoutForm.jsx
@@ -0,0 +1,125 @@
+import "./CheckoutForm.css";
+import { Form } from "react-bootstrap";
+import { useState } from "react";
+
+export default function CheckoutForm() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <div className="checkoutFormMainWrapper">
+      <div className="checkoutForm">
+        <div className="checkoutFormWrapper">
+          <Form>
+            <div className="checkoutForm1ContentsWrapper">
+              <h4 className="pb-3 pt-1">shipping details</h4>
+              <Form.Group className="mb-3">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="House number and street name"
+                />
+              </Form.Group>
+              <div className="row">
+                <div className="col-md-6 col-sm-6 col-12">
+                  <Form.Group className="mb-3">
+                    <Form.Label>Town / City *</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                </div>
+                <div className="col-md-6 col-sm-6 col-12">
+                  <Form.Group className="mb-3">
+                    <Form.Label>State *</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6 col-sm-6 col-12">
+                  <Form.Group className="mb-3">
+                    <Form.Label>ZIP *</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                </div>
+                <div className="col-md-6 col-sm-6 col-12">
+                  <Form.Group className="mb-3">
+                    <Form.Label>Phone *</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                </div>
+              </div>
+              <Form.Group className="mb-3">
+                <Form.Label>Order notes (optional)</Form.Label>
+                <Form.Control as="textarea" rows={4} cols={30} />
+              </Form.Group>
+            </div>
+            {checked === false && (
+              <div
+                className={
+                  "checkoutForm2ContentsWrapper animate__animated animate__fadeInLeft"
+                }
+              >
+                <h4 className="pb-3 pt-1">billing details</h4>
+                <Form.Group className="mb-3">
+                  <Form.Label>Full Name</Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="House number and street name"
+                  />
+                </Form.Group>
+                <div className="row">
+                  <div className="col-md-6 col-sm-6 col-12">
+                    <Form.Group className="mb-3">
+                      <Form.Label>Town / City *</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6 col-sm-6 col-12">
+                    <Form.Group className="mb-3">
+                      <Form.Label>State *</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6 col-sm-6 col-12">
+                    <Form.Group className="mb-3">
+                      <Form.Label>ZIP *</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-md-6 col-sm-6 col-12">
+                    <Form.Group className="mb-3">
+                      <Form.Label>Phone *</Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </div>
+                </div>
+                <Form.Group className="mb-3">
+                  <Form.Label>Order notes (optional)</Form.Label>
+                  <Form.Control as="textarea" rows={4} cols={30} />
+                </Form.Group>
+              </div>
+            )}
+            <Form.Group className="mb-3">
+              <Form.Check
+                id="checkoutCheckbox"
+                defaultChecked
+                checked={checked}
+                type="checkbox"
+                label="Billing details same as Shipping details."
+                onChange={() => setChecked(!checked)}
+              />
+            </Form.Group>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Checkout/CheckoutMain.jsx
+++ src/components/Checkout/CheckoutMain.jsx
@@ -1,19 +1,22 @@
 import "./CheckoutMain.css";
 import CartBreadcrumb from "../Cart/CartBreadcrumb";
 import CheckoutTotal from "./CheckoutTotal";
+import CheckoutForm from "./CheckoutForm";
 
 export default function CheckoutMain() {
   return (
     <div className="checkoutMainWrapper">
-      <div className="container-fluid">
+      <div className="container">
         <div className="row mt-5">
           <div className="col-md-12 d-flex jc-c">
             <CartBreadcrumb />
           </div>
         </div>
         <div className="row mt-5 mb-5">
-          <div className="col-md-8">col-8</div>
-          <div className="col-md-4">
+          <div className="col-md-7 col-lg-7 pe-lg-4">
+            <CheckoutForm />
+          </div>
+          <div className="col-md-4 col-lg-5">
             <CheckoutTotal />
           </div>
         </div>
