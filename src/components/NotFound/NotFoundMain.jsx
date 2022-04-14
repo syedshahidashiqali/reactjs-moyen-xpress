eef8b4228963ca33d28c8564b3b36f00b287d1a5
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-04-14 16:51:27 +0500

    complete error component with responsiveness

--- src/components/NotFound/NotFoundMain.jsx
+++ src/components/NotFound/NotFoundMain.jsx
@@ -1,20 +1,36 @@
-import "./NotFoundMain.css";
+import "./NotFoundMain.scss";
 import notFoundImg from "../../images/errorPageImage.png";
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function NotFoundMain() {
   return (
     <div className="notFoundMainWrapper">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-12">
+          <div className="col-md-12 text-center">
             <div className="notFoundBannerWrapper">
               <figure>
-                <img src={notFoundImg} alt="Not Found Image" />
+                <img
+                  className="img-fluid"
+                  src={notFoundImg}
+                  alt="Not Found Image"
+                />
               </figure>
               <div className="notFoundBannerText">
-                <h2>Oops!!! Something Went Wrong Here</h2>
-                <p></p>
+                <h2>
+                  <span>Oops!!!</span> Something Went Wrong Here
+                </h2>
+                <p>
+                  There may be a misspelling in the URL entered, or the page you
+                  are looking for may no longer exist
+                </p>
+                <Link to="/">
+                  <Button>
+                    go back home
+                    <i className="fa-solid fa-arrow-right ms-2" />
+                  </Button>
+                </Link>
               </div>
             </div>
           </div>
